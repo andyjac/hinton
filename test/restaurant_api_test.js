8ada25127dde457f7ba9eefe04b2f453d184c0a8
Lamson Le <ll217@u.washington.edu>
2015-05-18 16:24:24 -0700

    add test for finding single restaurant

--- test/restaurant_api_test.js
+++ test/restaurant_api_test.js
@@ -32,6 +32,22 @@ describe('restaurant REST API', function () {
 
       this.testRest = data;
     }.bind(this));
+
+    var testRest2 = new Rest({
+      name: 'Code Fellows',
+      address: {
+        number: '511',
+        street: 'Boren Avenue North',
+        city: 'Seattle',
+        state: 'WA',
+        zip: '98109',
+      }
+    });
+    testRest2.save(function (err, data) {
+      if (err) throw err;
+
+      this.testRest2 = data;
+    }.bind(this));
     done();
   });
 
@@ -48,6 +64,17 @@ describe('restaurant REST API', function () {
       expect(err).to.eql(null);
       expect(typeof res.body).to.eql('object');
       expect(Array.isArray(res.body)).to.eql(true);
+      done();
+    });
+  });
+
+  it('should get a restaurant by id', function (done) {
+    chai.request('localhost:3000')
+    .get('/api/restaurant/' + this.testRest2._id)
+    .end(function (err, res) {
+      expect(err).to.eql(null);
+      expect(res.status).to.eql(200);
+      expect(res.body.name).to.eql('Code Fellows');
       done();
     });
   });

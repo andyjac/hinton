fd60463545cf600af446cd3962c0d52c001d7559
Nick Eagan <neagan26@gmail.com>
2015-05-18 15:25:33 -0700

    add routes to server

--- server.js
+++ server.js
@@ -2,10 +2,21 @@
 
 var mongoose = require('mongoose');
 var express = require('express');
-
+var passport = require('passport');
 var app = express();
 
+process.env.APP_SECRET = process.env.APP_SECRET || 'ginahintonsfoodapp' // change
+
+var adminRoutes = express.Router();
+var restaurantRoutes = express.Router();
+
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/food_critic_dev');
+
+require('./routes/admin_routes.js')(adminRoutes);
+require('./routes/restaurant_routes.js')(restaurantRoutes);
+
+app.use('/api', adminRoutes);
+app.use('/api', restaurantRoutes);
 
 app.listen(3000, function() {
   console.log('server running on port ' + (process.env.PORT || 3000));

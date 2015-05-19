95d44c0b82276a47323168bc41cb5326f7f90382
bullitbd <bobd@rialtoadvisors.com>
2015-05-19 14:59:48 -0700

    simple admin update

--- admin/admin_form_Tue.js
+++ admin/admin_form_Tue.js
@@ -16,17 +16,20 @@ $(function() {
     // POST SUBMIT
     $('#buttonSave').click(function() {
 
-        var requestBody = {a:"b"};//buildRequest();
+        var requestBody = buildRequest();
         postData(requestBody); // return handled in ajax request
-        //alert( "Handler for .click() called." );
+        alert( "Handler for .click() called." );
     });
 
-
+     // CLEAR FORM
+    $('#buttonClear').click(function() {
+        $('#form1')[0].reset();
+    })
 
     function postData(data) {
         $.ajax({
             url: 'http://httpbin.org/post',      //'http://localhost:3000/api/restaurant',
-            contentType:'application/json',
+            dataType:'json',
             data: JSON.stringify(data),
             type: 'POST',
             context: document.body.form1,
@@ -43,37 +46,39 @@ $(function() {
     }
 
 
-    // function buildRequest() {
-    //     var Request = {
-    //     name: $('#r_name').value,
-    //     address: {
-    //       number: $('#r_number').value, //regex?
-    //       street: $('#r_street'),
-    //       city: $('#r_city'), //combo?
-    //       state: $('#r_state'), //verified by input box
-    //       zip: $('#r_zip'), // regex here
-    //     },
-    //     hours: {
-    //       // mon: $('#mon_open').value + $('#mon_am').value + '-' + $('#mon_close').value + $('#mon_pm').value;
-    //       // tue: $('#tue_open').value + $('#tue_am').value + '-' + $('#tue_close').value + $('#tue_pm').value;
-    //       // wed: $('#wed_open').value + $('#wed_am').value + '-' + $('#wed_close').value + $('#wed_pm').value;
-    //       // thu: $('#thu_open').value + $('#thu_am').value + '-' + $('#thu_close').value + $('#thu_pm').value;
-    //       // fri: $('#fri_open').value + $('#fri_am').value + '-' + $('#fri_close').value + $('#fri_pm').value;
-    //       // sat: $('#sat_open').value + $('#sat_am').value + '-' + $('#sat_close').value + $('#sat_pm').value;
-    //       // sun: $('#sun_open').value + $('#sun_am').value + '-' + $('#sun_close').value + $('#sun_pm').value;
-    //     },
-    //     phone: checkTel$('#r_phone').value, //do regex match here
-    //     genre: $('#r_genre').value, //verified in dropdown
-    //     price: calcPrice($('#r_price').value),
-    //     blog_link: checkUrl($('#r_blog').value),
-    //     r_site: checkUrl($('#r_site').value),
-    //     menu_link: checkUrl($('#r_menu').value),
-    //     menu_item: checkUrl($('#r_item').value),
-    //     photos: [],
-    //     other: {}
-    //     }
-    //     return request;
-    //}
+function buildRequest() {
+    var request = {
+    name: $('#r_name').val(),
+    address: {
+      number: $('#r_addr').val(), //regex?
+      street: $('#r_street').val(),
+      city: $('#r_city').val(), //combo?
+      state: $('#r_state').val(), //verified by input box
+      zip: $('#r_zip').val() // regex here
+    },
+    hours: {
+      mon: $('#mon_open').val() + $('#mon_am').val() + '-' + $('#mon_close').val() + $('#mon_pm').val(),
+      tue: $('#tue_open').val() + $('#tue_am').val() + '-' + $('#tue_close').val() + $('#tue_pm').val(),
+      wed: $('#wed_open').val() + $('#wed_am').val() + '-' + $('#wed_close').val() + $('#wed_pm').val(),
+      thu: $('#thu_open').val() + $('#thu_am').val() + '-' + $('#thu_close').val() + $('#thu_pm').val(),
+      fri: $('#fri_open').val() + $('#fri_am').val() + '-' + $('#fri_close').val() + $('#fri_pm').val(),
+      sat: $('#sat_open').val() + $('#sat_am').val() + '-' + $('#sat_close').val() + $('#sat_pm').val(),
+      sun: $('#sun_open').val() + $('#sun_am').val() + '-' + $('#sun_close').val() + $('#sun_pm').val()
+    },
+    phone: $('#r_tel').val(), //do regex match here
+    genre: $('#r_genre').val(), //verified in dropdown
+    price: $('#r_price').val(),
+    blog_link: $('#r_blog').val(),
+    r_site: $('#r_site').val(),
+    menu_link: $('#r_menu').val(),
+    menu_item: $('#r_item').val(),
+    photos: [],
+    other: {}
+    }
+    console.log($('#r_site'));
+    console.log(request);
+    return request;
+}
 
     // // temporary logic for daily hours strings
     // function openTime(day) {

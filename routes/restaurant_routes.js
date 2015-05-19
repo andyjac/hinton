0aa8bbc9d5aa85a219355e7545b0499921ca751b
Nick Eagan <neagan26@gmail.com>
2015-05-19 13:28:57 -0700

    add put and delete

--- routes/restaurant_routes.js
+++ routes/restaurant_routes.js
@@ -2,6 +2,7 @@
 
 var Rest = require('../models/Restaurant');
 var bodyparser = require('body-parser');
+var eatAuth = require('../lib/eat_auth')(process.env.APP_SECRET);
 
 module.exports = function (router) {
   router.use(bodyparser.json());
@@ -23,6 +24,33 @@ module.exports = function (router) {
         return res.status(500).json({msg: 'internal server error'});
       }
       res.status(200).json(data);
+    });
+  });
+
+  router.post('/restaurant', eatAuth, function(req, res) {
+
+  });
+
+  router.put('/restaurant/:id', eatAuth, function(req, res) {
+    var update = req.body;
+    delete update._id;
+
+    Rest.update({'_id': req.params.id}, update, function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({msg: 'internal server error'});
+      }
+      res.status(200).json({msg: 'update successful'});
+    });
+  });
+
+  router.delete('/restaurant/:id', eatAuth, function(req, res) {
+    Rest.remove({'_id': req.params.id}, function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({msg: 'internal server error'});
+      }
+      res.status(200).json({msg: 'delete successful'});
     });
   });
 

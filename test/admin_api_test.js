d79865cc04f41a9e5ecff2115989106d7ce5c81f
Andrew Jacobson <andyjacuw@gmail.com>
2015-05-18 22:02:57 -0700

    Clean up and refactor

--- test/admin_api_test.js
+++ test/admin_api_test.js
@@ -13,8 +13,21 @@ chai.use(chaihttp);
 var expect = chai.expect;
 
 describe('admin user creation and authentication', function() {
+  var password = bcrypt.hashSync('foobaz', bcrypt.genSaltSync(8), null);
 
-  afterEach(function(done) {
+  before(function(done) {
+    var testAdmin = new Admin({
+      username: 'test',
+      basic: { password: password, email: 'test2@example.com' }
+    });
+
+    testAdmin.save(function(err, user) {
+      if (err) console.log(err);
+      done();
+    });
+  });
+
+  after(function(done) {
     mongoose.connection.db.dropDatabase(function() {
       done();
     });
@@ -31,30 +44,14 @@ describe('admin user creation and authentication', function() {
       });
   });
 
-  describe('needs an existing user', function() {
-    var password = bcrypt.hashSync('foobaz', bcrypt.genSaltSync(8), null);
-
-    before(function(done) {
-      var testAdmin = new Admin({
-        username: 'test',
-        basic: { password: password, email: 'test@example.com' }
-      });
-
-      testAdmin.save(function(err, user) {
-        if (err) console.log(err);
+  it('should be able to sign in an admin user', function(done) {
+    chai.request('localhost:3000')
+      .get('/api/admin/sign_in')
+      .auth('test2@example.com', 'foobaz')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.body).to.have.property('token');
         done();
       });
-    });
-
-    it('should be able to sign in an admin user', function(done) {
-      chai.request('localhost:3000')
-        .get('/api/admin/sign_in')
-        .auth('test@example.com', 'foobaz')
-        .end(function(err, res) {
-          expect(err).to.eql(null);
-          expect(res.body).to.have.property('token');
-          done();
-        });
-    });
   });
 });

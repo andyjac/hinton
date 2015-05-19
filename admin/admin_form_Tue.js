84b6bda3f32a6c2669134755c310d4a4e749c831
bullitbd <bobd@rialtoadvisors.com>
2015-05-19 15:16:14 -0700

    redo of previous /admin commit with cleanup

--- admin/admin_form_Tue.js
+++ admin/admin_form_Tue.js
@@ -2,17 +2,6 @@
 
 $(function() {
 
-    //populate bound form data - Names...  populate state dropdown...
-
-    // $('#buttonAdd').click(function {
-    //     //on click opens the form for adding a new entry, positions & focus, etc.
-    //     //enables save button...
-    //     // if form is dirty, ask...
-    //     // clear inputs
-
-    // });
-
-
     // POST SUBMIT
     $('#buttonSave').click(function() {
 
@@ -24,7 +13,7 @@ $(function() {
      // CLEAR FORM
     $('#buttonClear').click(function() {
         $('#form1')[0].reset();
-    })
+    });
 
     function postData(data) {
         $.ajax({
@@ -46,54 +35,37 @@ $(function() {
         });
     }
 
-
-function buildRequest() {
-    var request = {
-    name: $('#r_name').val(),
-    address: {
-      number: $('#r_addr').val(), //regex?
-      street: $('#r_street').val(),
-      city: $('#r_city').val(), //combo?
-      state: $('#r_state').val(), //verified by input box
-      zip: $('#r_zip').val() // regex here
-    },
-    hours: {
-      mon: $('#mon_open').val() + $('#mon_am').val() + '-' + $('#mon_close').val() + $('#mon_pm').val(),
-      tue: $('#tue_open').val() + $('#tue_am').val() + '-' + $('#tue_close').val() + $('#tue_pm').val(),
-      wed: $('#wed_open').val() + $('#wed_am').val() + '-' + $('#wed_close').val() + $('#wed_pm').val(),
-      thu: $('#thu_open').val() + $('#thu_am').val() + '-' + $('#thu_close').val() + $('#thu_pm').val(),
-      fri: $('#fri_open').val() + $('#fri_am').val() + '-' + $('#fri_close').val() + $('#fri_pm').val(),
-      sat: $('#sat_open').val() + $('#sat_am').val() + '-' + $('#sat_close').val() + $('#sat_pm').val(),
-      sun: $('#sun_open').val() + $('#sun_am').val() + '-' + $('#sun_close').val() + $('#sun_pm').val()
-    },
-    phone: $('#r_tel').val(), //do regex match here
-    genre: $('#r_genre').val(), //verified in dropdown
-    price: $('#r_price').val(),
-    blog_link: $('#r_blog').val(),
-    r_site: $('#r_site').val(),
-    menu_link: $('#r_menu').val(),
-    menu_item: $('#r_item').val(),
-    photos: [],
-    other: {}
-    }
-    console.log($('#r_site'));
-    console.log(request);
+    function buildRequest() {
+        var request = {
+        name: $('#r_name').val(),
+        address: {
+          number: $('#r_addr').val(), //regex?
+          street: $('#r_street').val(),
+          city: $('#r_city').val(), //combo?
+          state: $('#r_state').val(), //verified by input box
+          zip: $('#r_zip').val() // regex here
+        },
+        hours: {
+          mon: $('#mon_open').val() + $('#mon_am').val() + '-' + $('#mon_close').val() + $('#mon_pm').val(),
+          tue: $('#tue_open').val() + $('#tue_am').val() + '-' + $('#tue_close').val() + $('#tue_pm').val(),
+          wed: $('#wed_open').val() + $('#wed_am').val() + '-' + $('#wed_close').val() + $('#wed_pm').val(),
+          thu: $('#thu_open').val() + $('#thu_am').val() + '-' + $('#thu_close').val() + $('#thu_pm').val(),
+          fri: $('#fri_open').val() + $('#fri_am').val() + '-' + $('#fri_close').val() + $('#fri_pm').val(),
+          sat: $('#sat_open').val() + $('#sat_am').val() + '-' + $('#sat_close').val() + $('#sat_pm').val(),
+          sun: $('#sun_open').val() + $('#sun_am').val() + '-' + $('#sun_close').val() + $('#sun_pm').val()
+        },
+        phone: $('#r_tel').val(), //do regex match here
+        genre: $('#r_genre').val(), //verified in dropdown
+        price: $('#r_price').val(),
+        blog_link: $('#r_blog').val(),
+        r_site: $('#r_site').val(),
+        menu_link: $('#r_menu').val(),
+        menu_item: $('#r_item').val(),
+        photos: [],
+        other: {}
+        }
+        //console.log($('#r_site'));
+        //console.log(request);
     return request;
-}
-
-    // // temporary logic for daily hours strings
-    // function openTime(day) {
-    //     var r_mon = $('#mon_open') + $('#mon_am') + '-' + $('#mon_close') + $('#mon_pm');
-    //     return
-    // }
-    // // calculate price to price category
-    // var incAmt = 10;
-    // function calcPrice(p) {
-    //     for (var i=1; i<4; i++) {
-    //         if (p <= incAmt*i) {
-    //             return i;
-    //         }
-    //     }
-    //     return 4;
-    // }
+    }
 });

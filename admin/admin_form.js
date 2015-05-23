6f6db8a8d55c2a04d3b0d90d20bf80e644f7be92
bullitbd <bobd@rialtoadvisors.com>
2015-05-23 00:09:37 -0700

    admin hours, list sorts...

--- admin/admin_form.js
+++ admin/admin_form.js
@@ -1,7 +1,22 @@
 $(function() {
 
+// ============ CONFIG VARS ===============
+
+  var hourNodes = [
+  {id:'mon_open',time:'closed'},{id:'mon_close',time:'closed'},
+  {id:'tue_open',time:'11:00'},{id:'tue_close',time:'10:00'},
+  {id:'wed_open',time:'11:00'},{id:'wed_close',time:'10:00'},
+  {id:'thu_open',time:'11:00'},{id:'thu_close',time:'10:00'},
+  {id:'fri_open',time:'11:00'},{id:'fri_close',time:'11:30'},
+  {id:'sat_open',time:'11:00'},{id:'sat_close',time:'11:30'},
+  {id:'sun_open',time:'11:00'},{id:'sun_close',time:'10:00'}
+  ];
+
+// ========================================
+
   getGenres();
   getVenues();
+  fillTimes();
 
   $('#buttonSave').click(function() {
     var requestBody = buildRequest();
@@ -71,9 +86,10 @@ $(function() {
       dataType: 'text',
         success: function(response) {
         var res = jQuery.parseJSON(response);
+        res.sort();
         console.log(res);
         $('#lblResponse').html(res);
-        fillComboG(res);
+        fillGenres(res);
 
       },
       error: function(xhr, status, error) {
@@ -90,14 +106,11 @@ $(function() {
       dataType: 'text',
       success: function(response) {
         var res = jQuery.parseJSON(response);
-
-        // response.forEach(function() {
-
-        // }
-        //console.log(response);
+        res.sort(function(a,b) {
+          return a.map.caption - b.map.caption;
+        });
         $('#lblResponse').html(res);
-        //console.log(response);
-        fillComboV(res);
+        fillVenues(res);
 
       },
       error: function(xhr, status, error) {
@@ -107,26 +120,40 @@ $(function() {
     });
   }
 
-  function fillComboV(data) {
+function fillTimes() {
+  var times = ['6:00','6:15','6:30','6:45','7:00','7:15','7:30','7:45','8:00','8:15','8:30','8:45','9:00','9:15','9:30','9:45','10:00','10:15','10:30','10:45','11:00','11:15','11:30','11:45','12:00','12:15','12:30','12:45','1:00','1:15','1:30','1:45','2:00','2:15','2:30','2:45','3:00','3:15','3:30','3:45','4:00','4:15','4:30','4:45','5:00','5:15','5:30','5:45','6:00','closed'];
+  for ( var i = 0, len = hourNodes.length; i < len; i++) {
+    sel = document.getElementById(hourNodes[i].id);
+    for ( var j = 0, len2 = times.length; j < len2; j++) {
+      var opt = document.createElement('option');
+      if (times[j] === hourNodes[i].time) opt.selected = true;
+      opt.innerHTML = times[j];
+      opt.value = times[j];
+      sel.appendChild(opt);
+    }
+  }
+}
+
+  function fillVenues(data) {
     data.forEach(function(el) {
       var sel = document.getElementById('r_name');
       var option = document.createElement('option');
-      console.log(el.map.caption);
+      //console.log(el.map.caption);
       option.innerHTML = el.map.caption;
       option.value = el._id;
-      console.log(option);
+      //console.log(option);
       sel.appendChild(option);
     });
     $('#r_name').combobox();
   }
 
-  function fillComboG(data) {
+  function fillGenres(data) {
     data.forEach(function(value, index) {
       var sel = document.getElementById('r_genre');
       var option = document.createElement('option');
       option.innerHTML = value;
       option.value = value;
-      console.log(option);
+      //console.log(option);
       sel.appendChild(option);
     });
     $('#r_genre').combobox();

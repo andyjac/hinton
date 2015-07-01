6f32b88805887fd2d55cfa604d1746e12582de78
Lamson Le <ll217@u.washington.edu>
2015-07-01 14:33:31 -0700

    add webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.initConfig({
     jshint: {
       dev: {
-        src: ['*.js', 'routes/**/*.js', 'test/**/*.js', 'lib/**/*.js', 'models/**/*.js', 'admin/**/*.js']
+        src: ['*.js', 'routes/**/*.js', 'test/**/*.js', 'lib/**/*.js', 'models/**/*.js', 'admin/**/*.js', 'app/**/*.js']
       },
 
       options: {
@@ -28,10 +30,37 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.dev.src %>', '.jshintrc'],
       tasks: ['default']
+    },
+
+    webpack: {
+      client: {
+        // webpack options
+        entry: __dirname + '/app/js/client.js',
+        output: {
+            path: 'build/',
+            filename: 'bundle.js'
+        },
+        module: {
+          loaders: []
+        }
+      }
+    },
+
+    copy: {
+      html: {
+        cwd: 'app/',
+        expand: true,
+        flatten: false,
+        src: '**/*.html',
+        dest: 'build/',
+        filter: 'isFile'
+      }
     }
+
   });
 
   grunt.registerTask('lint', ['jshint:dev']);
   grunt.registerTask('test', ['simplemocha:dev']);
+  grunt.registerTask('build', ['webpack:client', 'copy:html']);
   grunt.registerTask('default', ['lint', 'test']);
 };

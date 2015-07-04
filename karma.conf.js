e8cb8ce60d771685b2cee6b24e524ef38f7a246e
Lamson Le <ll217@u.washington.edu>
2015-07-04 11:46:33 -0700

    add karma mocha reporter

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
@@ -60,6 +60,12 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: true,
+
+    plugins: [
+      'karma-jasmine',
+      'karma-mocha-reporter',
+      'karma-phantomjs-launcher'
+    ]
   });
 };

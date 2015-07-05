058561376edec6b91df7de2bd4c5f1516dfc2d6d
Andrew Jacobson <andyjacuw@gmail.com>
2015-07-05 16:18:56 -0700

    Add simple restaurant form controller

--- app/js/controllers/restaurant_form_controller.js
+++ app/js/controllers/restaurant_form_controller.js
@@ -1,8 +1,33 @@
 'use strict';
 
 module.exports = function(app) {
-  app.controller('restaurantFormController', ['$scope', function($scope) {
-    $scope.greeting = 'hello world';
+  app.controller('restaurantFormController', ['$scope', 'clearFields', '$timeout', function($scope, clearFields, $timeout) {
 
+    $scope.venue = {
+      name: '',
+      genres: [],
+      price: '',
+      menuItem: '',
+      address: ''
+    };
+
+    $scope.addGenre = function(genre) {
+      if (genre.trim() !== '') {
+        $scope.venue.genres.push(genre);
+        $scope.genre = '';
+      }
+
+      document.getElementById('r_genre').focus();
+    };
+
+    $scope.removeGenre = function(index) {
+      $scope.venue.genres.splice(index, 1);
+    };
+
+    $scope.submitForm = function() {
+      var restaurantInfo = $scope.venue;
+      console.log(restaurantInfo);
+      clearFields(restaurantInfo);
+    };
   }]);
 };

7da19baffd842a09f3d80850ca31164325b2d037
Andrew Jacobson <andyjacuw@gmail.com>
2015-07-05 16:18:02 -0700

    Add clear fields service

---
+++ app/js/services/clear_fields.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+  app.factory('clearFields', function() {
+    return function(obj) {
+      Object.keys(obj).forEach(function(key) {
+        if (Array.isArray(obj[key])) {
+          obj[key] = [];
+        } else if (typeof obj[key] === 'object') {
+          obj[key] = {};
+        } else {
+          obj[key] = '';
+        }
+      });
+    };
+  });
+};

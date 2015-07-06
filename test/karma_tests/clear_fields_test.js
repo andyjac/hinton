f1ef6e5e295335d79c763153db13d77e32837b46
Lamson Le <ll217@u.washington.edu>
2015-07-06 15:56:15 -0700

    add clear fields test

--- test/karma_tests/clear_fields_test.js
+++ test/karma_tests/clear_fields_test.js
@@ -23,16 +23,13 @@ describe('clear fields service', function() {
   });
 
   it('should empty keys in the object', function() {
-    expect(Array.isArray(testobj.arr)).toBe(true);
-    expect(typeof testobj.fun).toBe('function');
-    expect(typeof testobj.obj).toBe('object');
-    expect(typeof testobj.str).toBe('string');
     var emptyobj = clearFields(testobj);
     expect(Array.isArray(emptyobj.arr)).toBe(true);
     expect(emptyobj.arr.length).toBe(0);
     expect(typeof emptyobj.fun).toBe('string');
     expect(emptyobj.fun).toBe('');
     expect(typeof emptyobj.obj).toBe('object');
+    expect(Object.keys(emptyobj.obj).length).toBe(0);
     expect(typeof emptyobj.str).toBe('string');
     expect(emptyobj.str).toBe('');
   });

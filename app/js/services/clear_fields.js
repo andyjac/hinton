38230c80bf0bc3ddf4f07f8cc143c4ef2ca8f424
Andrew Jacobson <andyjacuw@gmail.com>
2015-07-27 22:12:58 -0700

    Simplify ifs

--- app/js/services/clear_fields.js
+++ app/js/services/clear_fields.js
@@ -9,9 +9,7 @@ module.exports = function(app) {
         var i = collection.length;
 
         while(i--) {
-          if (Array.isArray(collection[i])) {
-            collection[i] = clearFields(collection[i]);
-          } else if (typeof collection[i] === 'object') {
+          if (typeof collection[i] === 'object') {
             collection[i] = clearFields(collection[i]);
           } else {
             collection.splice(i, 1);
@@ -20,9 +18,7 @@ module.exports = function(app) {
       } else {
         Object.keys(collection).forEach(function(value) {
           if (collection.hasOwnProperty(value) && collection[value] !== null) {
-            if (Array.isArray(collection[value])) {
-              collection[value] = clearFields(collection[value]);
-            } else if (typeof collection[value] === 'object') {
+            if (typeof collection[value] === 'object') {
               collection[value] = clearFields(collection[value]);
             } else if (typeof collection[value] === 'number') {
               collection[value] = 0;

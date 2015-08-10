7d2adb7e9249bdf078f3709b7bd6e7da4014d82e
Andrew Jacobson <andyjacuw@gmail.com>
2015-08-10 01:52:39 -0700

    Rename userRoutes to adminRoutes

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ var app = express();
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/hinton_dev');
 process.env.APP_SECRET = process.env.APP_SECRET || 'hinton_dev';
 
-var userRoutes = express.Router();
+var adminRoutes = express.Router();
 var restaurantRoutes = express.Router();
 
 app.use(passport.initialize());
 
 require('./lib/passport_strat')(passport);
 
-require('./routes/user_routes.js')(userRoutes, passport);
+require('./routes/admin_routes.js')(adminRoutes, passport);
 require('./routes/restaurant_routes.js')(restaurantRoutes);
 
 app.use('/api', restaurantRoutes);
-app.use('/hinton', userRoutes);
+app.use('/admin', adminRoutes);
 app.use('/', express.static(__dirname + '/build'));
 
 app.all('*', function(req, res) {

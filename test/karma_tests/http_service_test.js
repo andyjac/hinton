18cc30e0c23f3702c3f5d579f39ac1f92fc95b7c
Lamson Le <ll217@u.washington.edu>
2015-08-14 16:16:13 -0700

    add test for token in headers

--- test/karma_tests/http_service_test.js
+++ test/karma_tests/http_service_test.js
@@ -6,12 +6,15 @@ require('angular-mocks');
 describe('http service', function() {
   var httpService;
   var $httpBackend;
+  var $cookies;
+  var $http;
 
   beforeEach(angular.mock.module('hintonAdminApp'));
 
-  beforeEach(angular.mock.inject(function(_httpService_, _$httpBackend_) {
+  beforeEach(angular.mock.inject(function(_httpService_, _$httpBackend_, _$cookies_) {
     httpService = _httpService_;
     $httpBackend = _$httpBackend_;
+    $cookies = _$cookies_;
   }));
 
   it('should be a service', function() {
@@ -20,6 +23,26 @@ describe('http service', function() {
     expect(typeof httpService('api').create).toBe('function');
     expect(typeof httpService('api').save).toBe('function');
     expect(typeof httpService('api').remove).toBe('function');
+  });
+
+  it('should not send token if none is set', function() {
+    $cookies.put('eat', '');
+    $httpBackend.whenGET('/admin/hinton', function(headers) {
+      expect(headers['eat']).toBe('');
+      return !headers.Authorization;
+    }).respond(200);
+    httpService('hinton').getAll(function(){});
+    $httpBackend.flush();
+  });
+
+  it('should send token in header', function() {
+    $cookies.put('eat', 'token123');
+    $httpBackend.expectGET('/admin/hinton', function(headers) {
+      expect(headers['eat']).toBe('token123');
+      return !headers.Authorization;
+    }).respond(200);
+    httpService('hinton').getAll(function(){});
+    $httpBackend.flush();
   });
 
 });

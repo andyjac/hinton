8afb83d35b6f33aa714274bf19be4cd6d5b472a4
Andrew Jacobson <andyjacuw@gmail.com>
2015-08-18 14:36:04 -0700

    Indentation

--- app/js/services/modal_service.js
+++ app/js/services/modal_service.js
@@ -1,54 +1,54 @@
 'use strict';
 
 module.exports = function(app) {
-    app.service('modalService', ['$modal', function ($modal) {
+  app.service('modalService', ['$modal', function ($modal) {
 
-        var modalDefaults = {
-            backdrop: true,
-            keyboard: true,
-            modalFade: true,
-            templateUrl: '../../partials/modal.html'
+    var modalDefaults = {
+      backdrop: true,
+      keyboard: true,
+      modalFade: true,
+      templateUrl: '../../partials/modal.html'
+    };
+
+    var modalOptions = {
+      closeButtonText: 'Close',
+      actionButtonText: 'OK',
+      headerText: 'Proceed?',
+      bodyText: 'Perform this action?'
+    };
+
+    this.showModal = function (customModalDefaults, customModalOptions) {
+      if (!customModalDefaults) customModalDefaults = {};
+      customModalDefaults.backdrop = 'static';
+      return this.show(customModalDefaults, customModalOptions);
+    };
+
+    this.show = function (customModalDefaults, customModalOptions) {
+      //Create temp objects to work with since we're in a singleton service
+
+      var tempModalDefaults = {};
+      var tempModalOptions = {};
+
+      //Map angular-ui modal custom defaults to modal defaults defined in service
+      angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
+
+      //Map modal.html $scope custom properties to defaults defined in service
+      angular.extend(tempModalOptions, modalOptions, customModalOptions);
+
+      if (!tempModalDefaults.controller) {
+        tempModalDefaults.controller = function ($scope, $modalInstance) {
+          $scope.modalOptions = tempModalOptions;
+          $scope.modalOptions.ok = function (result) {
+            $modalInstance.close(result);
+          };
+          $scope.modalOptions.close = function (result) {
+            $modalInstance.dismiss('cancel');
+          };
         };
+      }
 
-        var modalOptions = {
-            closeButtonText: 'Close',
-            actionButtonText: 'OK',
-            headerText: 'Proceed?',
-            bodyText: 'Perform this action?'
-        };
+      return $modal.open(tempModalDefaults).result;
+    };
 
-        this.showModal = function (customModalDefaults, customModalOptions) {
-            if (!customModalDefaults) customModalDefaults = {};
-            customModalDefaults.backdrop = 'static';
-            return this.show(customModalDefaults, customModalOptions);
-        };
-
-        this.show = function (customModalDefaults, customModalOptions) {
-            //Create temp objects to work with since we're in a singleton service
-
-            var tempModalDefaults = {};
-            var tempModalOptions = {};
-
-            //Map angular-ui modal custom defaults to modal defaults defined in service
-            angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
-
-            //Map modal.html $scope custom properties to defaults defined in service
-            angular.extend(tempModalOptions, modalOptions, customModalOptions);
-
-            if (!tempModalDefaults.controller) {
-                tempModalDefaults.controller = function ($scope, $modalInstance) {
-                    $scope.modalOptions = tempModalOptions;
-                    $scope.modalOptions.ok = function (result) {
-                        $modalInstance.close(result);
-                    };
-                    $scope.modalOptions.close = function (result) {
-                        $modalInstance.dismiss('cancel');
-                    };
-                };
-            }
-
-            return $modal.open(tempModalDefaults).result;
-        };
-
-    }]);
+  }]);
 };

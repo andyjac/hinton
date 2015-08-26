707eeba0542899535788d263ce0467e887b48a75
Bob Day <bobd@rialtoadvisors.com>
2015-08-26 14:45:32 -0700

    • fix typo in restaurant controller; • finish cleanup & function of thumb-directive • rename 'google address', show-if empty addr field • refactor upload_files view to gain file-drop functionality • various css tweeks

--- app/js/directives/thumb_directive.js
+++ app/js/directives/thumb_directive.js
@@ -2,28 +2,21 @@
 
 module.exports = function(app) {
   app.directive('thumbDirective', [function() {
-    console.log('got here...');
 
     return {
       restrict: 'A',
-      replace: true,
-      // declaring controller here throws a 'unknown injector' error
-      // controller: 'modalInstanceController',
+      replace: false,
       scope: {
         file: '='
       },
 
-      // template: '<img src="src">',
-      // controller: ['fileReader', function(fileReader) {
       link: function(scope, el, attrs) {
-        console.log(scope.file);
         var reader = new FileReader();
         reader.onload = function() {
            var filesrc = reader.result;
-          el.html('src="filesrc"');
+          el.css('background-image', 'url(' + filesrc + ')');
         };
         reader.readAsDataURL(scope.file);
-        scope.$apply(); // this throws a '$digest already in progress' error
       }
     };
   }]);

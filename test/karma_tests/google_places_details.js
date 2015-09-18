c3c56dedebd1c14d2581893d65763cd312a46e96
Bob Day <bobd@rialtoadvisors.com>
2015-09-17 22:01:39 -0700

    Update google_places_details.js
    Fix test to agree with updated location data

--- test/karma_tests/google_places_details.js
+++ test/karma_tests/google_places_details.js
@@ -37,8 +37,8 @@ module.exports = {
   "formatted_phone_number": "(206) 545-7440",
   "geometry": {
     "location": {
-      "G": 47.658506,
-      "K": -122.35033099999998
+      "H": 47.658506,
+      "L": -122.35033099999998
     }
   },
   "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
